33c6f42579eb0c7f87844c4cd2828840e18d8b81
benkim077 <123vfcx@gmail.com>
2023-01-17 22:52:27 +0900

    temp

--- src/domains/TodoList/TodoListTitle.vue
+++ src/domains/TodoList/TodoListTitle.vue
@@ -1,6 +1,8 @@
 <template lang="">
   <div>
-    <h1>TodoList Title</h1>
+    <h1 class="text-3xl font-bold underline">TodoList Title</h1>
+    <h1 class="custom_title">TITLE2</h1>
+    <h1 class="my_title">TITLE3</h1>
   </div>
 </template>
 <script setup></script>
