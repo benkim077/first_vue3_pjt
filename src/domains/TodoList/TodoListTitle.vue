b65845014c1063bf774fcb7151d9d93e80503e9a
benkim077 <123vfcx@gmail.com>
2023-01-17 23:46:48 +0900

    done

--- src/domains/TodoList/TodoListTitle.vue
+++ src/domains/TodoList/TodoListTitle.vue
@@ -3,7 +3,10 @@
     <h1 class="text-3xl font-bold underline">TodoList Title</h1>
     <h1 class="custom_title">TITLE2</h1>
     <h1 class="my_title">TITLE3</h1>
+    <h1 :class="$style.modules_title">TITLE 4</h1>
   </div>
 </template>
 <script setup></script>
-<style lang=""></style>
+<style module>
+@import "./TodoListTitle.css";
+</style>
